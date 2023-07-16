4a3500d75510afcb4d60b5030deea088a15a604d
unknown <jacqueline.21042007@gmail.com>
2023-07-16 09:57:20 -0600

    Creating data classes

---
+++ WinInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace TicTacToe
+{
+    public class WinInfo
+    {
+        public WinType Type { get; set; } 
+        public int Number { get; set; }
+    }
+}
---
+++ WinType.cs
@@ -0,0 +1,7 @@
+﻿namespace TicTacToe
+{
+    public enum WinType
+    {
+        Row, Column, MainDiagomal, AntiDiagonal
+    }
+}
