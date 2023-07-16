e54880e4dc4bccc3ba044c7210fb3810b42c861f
unknown <jacqueline.21042007@gmail.com>
2023-07-16 14:24:59 -0600

    Loading Images

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,9 +18,26 @@ namespace TicTacToe
 {
     public partial class MainWindow : Window
     {
+
+        private readonly Dictionary<Player, ImageSource> ImaageSource = new()
+        {
+            {Player.X, new BitmapImage(new Uri("pack://application:,,,/Assets/X15.png")) },
+            {Player.O, new BitmapImage(new Uri("pack://application:,,,/Assets/O15.png")) }
+        };
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GameGrid_Mouse(Object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
